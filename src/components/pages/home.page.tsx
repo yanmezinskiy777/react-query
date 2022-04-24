42e80d1c99c255b14a7b6e06623f4220a710d1b2
yan.mezinskiy <yan.mezinskiy@sm-analytics.com>
2022-04-24 10:21:31 +0300

    cb success error

--- src/components/pages/home.page.tsx
+++ src/components/pages/home.page.tsx
@@ -1,21 +1,36 @@
 import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
 import axios from "axios";
 import { useQuery } from "react-query";
 import { IUserData } from "./types";
+
+type TUser = Partial<IUserData>;
 
 const fetchUsers = () => {
   return axios.get<IUserData[]>("https://jsonplaceholder.typicode.com/users");
 };
 
 const HomePage = () => {
-  const { isError, isLoading, data, status, isFetching } = useQuery(
+  const onSuccess = (data: any) => {
+    console.log("Success fetching data");
+    console.log(data);
+  };
+
+  const onError = (error: any) => {
+    console.dir("Error fetching");
+    console.dir(error);
+  };
+
+  const { isError, isLoading, data, status, isFetching, refetch } = useQuery(
     "query-users",
     fetchUsers,
     {
-      staleTime: 10000
+      enabled: false,
+      onSuccess,
+      onError,
     }
   );
-  console.log(isFetching, isLoading);
+  console.log({ isFetching, isLoading });
   if (isError) {
     return <h1>{status}</h1>;
   }
@@ -24,10 +39,21 @@ const HomePage = () => {
     return <h1>Loading...</h1>;
   }
 
+  const getUsers = () => refetch();
+
   return (
     <div>
       <Typography variant="h1">Home Page</Typography>
-      {data?.data.map((user) => (
+      <Button
+        onClick={getUsers}
+        variant="contained"
+        component="button"
+        color="primary"
+        size="medium"
+      >
+        Get Users
+      </Button>
+      {data?.data.map((user: TUser) => (
         <div key={user.id}>{user.username}</div>
       ))}
     </div>
